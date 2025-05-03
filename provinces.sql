7ceed10e39d6ee98a6684d2c00ce9832d22a9090
Nnyny <nguonnynytan@gmail.com>
2025-05-03 17:33:49 +0700

    initial commit

---
+++ provinces.sql
@@ -0,0 +1,32 @@
+CREATE TABLE provinces (
+    type	VARCHAR(255),
+    code	INT,
+    name_km	VARCHAR(255),
+    name_en	VARCHAR(255)
+);
+
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '1', 'បន្ទាយមានជ័យ', 'Banteay Meanchey');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '2', 'បាត់ដំបង', 'Battambang');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '3', 'កំពង់ចាម', 'Kampong Cham');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '4', 'កំពង់ឆ្នាំង', 'Kampong Chhnang');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '5', 'កំពង់ស្ពឺ', 'Kampong Speu');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '6', 'កំពង់ធំ', 'Kampong Thom');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '7', 'កំពត', 'Kampot');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '8', 'កណ្ដាល', 'Kandal');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '9', 'កោះកុង', 'Koh Kong');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '10', 'ក្រចេះ', 'Kratie');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '11', 'មណ្ឌលគិរី', 'Mondul Kiri');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '12', 'រាជធានីភ្នំពេញ', 'Phnom Penh');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '13', 'ព្រះវិហារ', 'Preah Vihear');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '14', 'ព្រៃវែង', 'Prey Veng');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '15', 'ពោធិ៍សាត់', 'Pursat');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '16', 'រតនគិរី', 'Ratanak Kiri');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '17', 'សៀមរាប', 'Siemreap');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '18', 'ព្រះសីហនុ', 'Preah Sihanouk');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '19', 'ស្ទឹងត្រែង', 'Stung Treng');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '20', 'ស្វាយរៀង', 'Svay Rieng');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '21', 'តាកែវ', 'Takeo');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '22', 'ឧត្ដរមានជ័យ', 'Oddar Meanchey');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '23', 'កែប', 'Kep');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '24', 'ប៉ៃលិន', 'Pailin');
+INSERT INTO provinces (type, code, name_km, name_en) VALUES ('ខេត្ត​', '25', 'ត្បូងឃ្មុំ', 'Tboung Khmum');
